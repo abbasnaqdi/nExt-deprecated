a6e610adfa325d9be0b40760b9e68c259e4f2589
oky2abbas <naqqdi@gmail.com>
2020-03-04 00:00:26 +0330

    add support logV,E,I,D,W,WTF

--- library/src/main/java/com/github/oky2abbas/nExt/basic/Collection.kt
+++ library/src/main/java/com/github/oky2abbas/nExt/basic/Collection.kt
@@ -3,24 +3,24 @@ package com.github.oky2abbas.nExt.basic
 
 fun <T> List<T>.printList() {
     this.forEachIndexed { index, t ->
-        "$index -> ".printInfo(t.toString())
+        "$index -> ".logE(t.toString())
     }
 }
 
 fun <T> Array<T>.printList() {
     this.forEachIndexed { index, t ->
-        "$index -> ".printInfo(t.toString())
+        "$index -> ".logE(t.toString())
     }
 }
 
 fun <K, V> Map<K, V>.printMap() {
     this.forEach {
-        "${it.key} -> ${it.value}".printInfo(this.toString())
+        "${it.key} -> ${it.value}".logE(this.toString())
     }
 }
 
 fun <K, V> HashMap<K, V>.printMap() {
     this.forEach {
-        "${it.key} -> ${it.value}".printInfo(this.toString())
+        "${it.key} -> ${it.value}".logE(this.toString())
     }
 }
