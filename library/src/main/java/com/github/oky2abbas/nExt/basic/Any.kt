25797080199a8fe8356866039a185e6e0b8e0485
oky2abbas <naqqdi@gmail.com>
2020-05-03 18:21:53 +0430

    improve tag in logs

--- library/src/main/java/com/github/oky2abbas/nExt/basic/Any.kt
+++ library/src/main/java/com/github/oky2abbas/nExt/basic/Any.kt
@@ -2,26 +2,26 @@ package com.github.oky2abbas.nExt.basic
 
 import android.util.Log
 
-fun Any.logV(tag: String = "nExt VERBOSE ->", throwable: Throwable? = null) {
-    Log.v(tag, "$this\n", throwable)
+fun Any.logV(tag: String = "", throwable: Throwable? = null) {
+    Log.v("nExt -> $tag", "$this\n", throwable)
 }
 
-fun Any.logE(tag: String = "nExt ERROR ->", throwable: Throwable? = null) {
-    Log.e(tag, "$this\n", throwable)
+fun Any.logE(tag: String = "", throwable: Throwable? = null) {
+    Log.e("nExt -> $tag", "$this\n", throwable)
 }
 
-fun Any.logI(tag: String = "nExt INFO ->", throwable: Throwable? = null) {
-    Log.i(tag, "$this\n", throwable)
+fun Any.logI(tag: String = "", throwable: Throwable? = null) {
+    Log.i("nExt -> $tag", "$this\n", throwable)
 }
 
-fun Any.logD(tag: String = "nExt DEBUG ->", throwable: Throwable? = null) {
-    Log.d(tag, "$this\n", throwable)
+fun Any.logD(tag: String = "", throwable: Throwable? = null) {
+    Log.d("nExt -> $tag", "$this\n", throwable)
 }
 
-fun Any.logW(tag: String = "nExt WARN ->", throwable: Throwable? = null) {
-    Log.w(tag, "$this\n", throwable)
+fun Any.logW(tag: String = "", throwable: Throwable? = null) {
+    Log.w("nExt -> $tag", "$this\n", throwable)
 }
 
-fun Any.logWTF(tag: String = "nExt WTF ->", throwable: Throwable? = null) {
-    Log.wtf(tag, "$this\n", throwable)
+fun Any.logWTF(tag: String = "", throwable: Throwable? = null) {
+    Log.wtf("nExt -> $tag", "$this\n", throwable)
 }
--- library/src/main/java/com/github/oky2abbas/nExt/basic/Collection.kt
+++ library/src/main/java/com/github/oky2abbas/nExt/basic/Collection.kt
@@ -1,21 +1,21 @@
 package com.github.oky2abbas.nExt.basic
 
 
-fun <T> Collection<T>.logListE(tag: String = "nExt -> ") {
+fun <T> Collection<T>.logListE(tag: String = "") {
     this.forEach {
         it?.logE(tag)
     }
 }
 
-fun <T> Array<T>.logArrayE(tag: String = "nExt -> ") {
+fun <T> Array<T>.logArrayE(tag: String = "") {
     this.forEach {
         it?.logE(tag)
     }
 }
 
-fun <K, V> Map<*, *>.logMapE(tag: String = "nExt -> ") {
+fun <K, V> Map<*, *>.logMapE(tag: String = "") {
     this.forEach {
-        this.logE("$tag${it.key}:${it.value}")
+        this.logE("$tag${it.key}: ${it.value}")
     }
 }
 
